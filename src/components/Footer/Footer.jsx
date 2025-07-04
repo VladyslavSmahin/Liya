c51244eb0645f3718d0784ff273f1df31e19bc34
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2025-07-04 16:35:44 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import './index.scss'
 
 export default function Footer() {
     return (
-        <header>
-            <nav >
-                <Link to="/">Обо мне</Link>
-                <Link to="/poems">Мои стихи</Link>
-                <Link to="/contact">Общение</Link>
-            </nav>
-        </header>
+        <div className='footerWrapper'>
+            <footer className='footer'>
+                <img src='../fav-icon.png' className='logo' alt='Logo'/>
+                <nav>
+                    <a href=''>Про мене</a>
+                    <a href=''>Мої вірші</a>
+                    <a href=''>Спілкування</a>
+                    <a href=''>Соц. мережі</a>
+                </nav>
+                <div className='contactTeam'>
+                    <p>Дизайн:<a href=''> miss.shaana</a></p>
+                    <p>Музика:<a href=''> USPENSKIY DRIVE</a>, <a href=''> Лія Юзько</a></p>
+                </div>
+                <p>Всі права захищені © 2025</p>
+            </footer>
+        </div>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,16 @@ import './index.scss'
 
 export default function Header() {
     return (
-        <header className={'header container'}>
-            <nav>
-                <a href=''>Про мене</a>
-                <a href=''>Мої вірші</a>
-                <img src='../fav-icon.png' className='logo' alt='Logo'/>
-                <a href=''>Спілкування</a>
-                <a href=''>Соц. мережі</a>
-            </nav>
-        </header>
+       <div className='headerWrapper'>
+           <header className={'header container'}>
+               <nav>
+                   <a href=''>Про мене</a>
+                   <a href=''>Мої вірші</a>
+                   <img src='../fav-icon.png' className='logo' alt='Logo'/>
+                   <a href=''>Спілкування</a>
+                   <a href=''>Соц. мережі</a>
+               </nav>
+           </header>
+       </div>
     )
 }
